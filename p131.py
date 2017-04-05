31af63eb3e9bfeff14f4ef414b5c38f7575cc68d
Ricardo Oda <odaric@ime.usp.br>
2017-04-05 01:57:41 -0300

    arrumando encode e comentários

--- p131.py
+++ p131.py
@@ -1,15 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from math import sqrt, pow
-from __builtin__ import True
+
+LIMIT = 1000000
 
 #------------------------------------------------
-# https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
-# encontrando primos
-LIMIT = 1000000
+"""
+Encontrando primos até 1000000
+https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+
+is_prime[x] devolve se x é primo ou não
+"""
 
 is_prime = [True]*LIMIT
 is_prime[0] = False
 is_prime[1] = False
-
 
 for i in xrange(2,int(sqrt(LIMIT))):
     if is_prime[i]:
@@ -19,15 +25,32 @@ for i in xrange(2,int(sqrt(LIMIT))):
             is_prime[x] = False
             j += 1
             x = i*j
-    
-primes = []    
-for i in xrange(2,LIMIT):
-    if is_prime[i]:
-        primes.append(i)
 
 #------------------------------------------------
+"""
+Algumas contas para resolver o problema:
 
+n³ + n²p = x³
+=> n²(n+p) = x³
 
+Daqui para termos um cubo perfeito, n e n+p devem ser cubos perfeitos também,
+senão n+p = n² para formar um cubo perfeito que é um absurdo.
+
+n = a³
+n+p = b³
+
+=> p = b³ - a³ = (a - b)(a² + ab + b²)
+
+Como p é primo ele não pode ser um produto então um dos fatores é 1.
+Como a²>=1 e b²>=1, temos que (a² + ab + b²) > 1 e (a-b) = 1.
+
+b = a + 1
+
+Dado que p = b³ - a³, temos que p é a diferença de cubos "vizinhos".
+"""
+
+# Testando primalidade de todas as diferenças de cubos "vizinhos" até 1000000
+# chegamos à resposta 173
 k = 1
 done = False
 answer = 0
--- p76.py
+++ p76.py
@@ -1,4 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 """
+Depois de quebrar a cabeça tentando achar a recurção que resolve o problema,
+dei uma perguntada pro tio Google e achei isso aqui:
+http://mathworld.wolfram.com/PartitionFunctionP.html
+
+#------------------------------------------------------------------------------
+...
 P(n,k) denotes the number of ways of writing n as a sum of exactly k terms or,
 equivalently, the number of partitions into parts of which the largest is
 exactly k. (Note that if "exactly k" is changed to "k or fewer" and
@@ -23,7 +32,49 @@ and P(n,0)=0. The triangle of P(k,n) is given by
 1  2  1  1             5
 1  2  2  1  1          7
 1  3  3  2  1  1      11
-... 
+...
+#------------------------------------------------------------------------------
+
+Tentando explicar:
+
+P(n,k) é o número de jeitos de se escrever n com k termos.
+Pelo que entendi podemos separá-lo em casos que contém o termo 1 e casos que não
+contém o termo 1.
+
+P(7,3)
+5 1 1
+4 2 1
+3 3 1 contém 1
+-----
+3 2 2 não contém 1
+
+Para casos que contém o termo um fazemos P(n-1, k-1) ou seja achamos como
+escrever n-1 com um termo a menos.
+
+P(6,2)
+5 1
+4 2
+3 3 (iguais ao do P(7,3) sem o termo 1 no final)
+
+
+Para casos que não contém o 1, é possível subtrair 1 de todos os termos, e assim
+subtrair o número de termos (k) do total (n) e teremos uma combinação P(n-k,k)
+que já foi calculada anteriormente.
+
+P(4,3)
+2 1 1 (somando 1 em cada termo voltamos à o caso que não contém 1 de P(7,3)) 
+
+Então por isso a fórmula é:
+P(n, k) = P(n-1, k-1) + P(n-k, k)
+
+#------------------------------------------------------------------------------
+
+P(100) = 190569292
+
+Lembrando que temos que subtrair 1 por não estarmos contando o caso com 1 termo
+para o problema.
+
+Então a resposta é 190569291 
 """
 
 
@@ -33,16 +84,14 @@ p = {}
 
 for n in xrange(2,x+1):
     sum = 0
-    s = ""
     for k in xrange(1,n+1):
         if k in [1,n]:
             p[(n,k)] = 1
         elif k <= n-k:
             p[(n,k)] = p[(n-1,k-1)] + p[(n-k),k]
-        else:
+        else: #k > n-k
             p[(n,k)] = p[(n-1,k-1)]
         sum += p[(n,k)]
-        s += " "+str(p[(n,k)])
-    print "(%3d): %3d | %s" % (n, sum, s)
+print sum
 
         
